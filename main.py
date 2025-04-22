137945be4a2a703e3a8cd14a28764fe83bf7c6a9
Adam Lusch <alusch@gmail.com>
2025-04-21 21:16:59 -0500

    Basic driver

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+import ast
+import pathlib
+
+import pandas as pd
+
+import timeline_generator
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a timeline from a CSV")
+    parser.add_argument("in_file", metavar="CSV", type=pathlib.Path, help="Input CSV file")
+    parser.add_argument("out_file", metavar="OUTPUT", type=pathlib.Path, help="Output image file to write")
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.in_file)
+    df['options'] = df['options'].apply(ast.literal_eval)
+    timeline_generator.get_timeline(df, filename=args.out_file, granularity='years', interval=5, dateformat='%Y')
+
+
+if __name__ == "__main__":
+    main()
